4f2ae524e2a199b17dddbdca12431f049a064492
Yash Bhandari <133970746+bhandariyash76@users.noreply.github.com>
2026-04-14 18:29:11 +0530

    Update Problem2_Lecture2.py

--- Problem2_Lecture2.py
+++ Problem2_Lecture2.py
@@ -12,4 +12,37 @@ elif 70 > marks >= 60:
     print("Grade: D")
 else:
     print("Grade: F")
-    
+
+
+# WAP to check if a number is even or odd
+
+number = int(input("enter the number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+# WAP to find the greatest of 3 numbers entered by the user
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    greatest = num1
+elif num2 >= num1 and num2 >= num3:
+    greatest = num2
+else:   
+    greatest = num3
+
+print("The greatest number is:", greatest)
+
+#WAP to check if a number is a multiplle of 7 or not
+
+num = int(input("Enter a number: "))
+
+if num % 7 == 0:
+    print(num, "is a multiple of 7.")
+else:
+    print(num, "is not a multiple of 7.")
