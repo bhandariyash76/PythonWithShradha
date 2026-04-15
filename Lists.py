60f1b387680d1cf3943ff192eadfe361a086401e
Yash Bhandari <133970746+bhandariyash76@users.noreply.github.com>
2026-04-15 20:47:52 +0530

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,9 @@
+# Lists in python : a built in data structure that can hold an ordered collection of items, which can be of different types. Lists are mutable, meaning you can change their content after they have been created.
+
+# Creating a list
+
+my_list = [1, 2, 3, 'hello', True] # list can contain different types of data
+
+# Accessing elements in a list
+print(my_list[0]) # Output: 1
+print(my_list[3]) # Output: 'hello'
