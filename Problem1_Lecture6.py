a409fa74dcd43c9f78b2122348975b6bbdca019f
Yash Bhandari <133970746+bhandariyash76@users.noreply.github.com>
2026-04-27 22:45:13 +0530

    Create Problem1_Lecture6.py

---
+++ Problem1_Lecture6.py
@@ -0,0 +1,28 @@
+# WAP to print the length of a list using a function.
+
+def list_length(lst):
+    count = 0
+    for item in lst:
+        count += 1
+    return count
+
+# WAP to print the elements of a list in a single line using a function.
+
+def print_list(lst):
+    for item in lst:
+        print(item, end=" ")
+    return 0
+
+# WAP to find the factorial of n using a function.
+
+def factorial (n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+    
+# WAP to convert usd to inr using a function.
+
+def converter(usd):
+    return usd * 82.74
+
